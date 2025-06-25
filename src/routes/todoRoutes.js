8e9e11a9db4e4423a8005c8678ecf48dc72d07e5
Murali Mohan Reddy <nagamuralimohan12@gmail.com>
2025-06-25 14:24:19 +0530

    Created Todo

---
+++ src/routes/todoRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createTodo } from "../controllers/todoController.js";
+
+
+const router = express.Router();
+
+router.post('/' ,createTodo);
+
+export default router;
